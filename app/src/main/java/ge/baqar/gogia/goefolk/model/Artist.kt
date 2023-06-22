b40d6e73cf2e7f04bb7c80497f5a6d3e467affeb
pirati02 <b.gogia94@gmail.com>
2023-06-22 19:23:38 +0400

    fix opening old recording

--- app/src/main/java/ge/baqar/gogia/goefolk/model/Artist.kt
+++ app/src/main/java/ge/baqar/gogia/goefolk/model/Artist.kt
@@ -16,6 +16,25 @@ data class Artist(
     override fun detailedName(): String {
         return name
     }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Artist
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (nameEng != other.nameEng) return false
+        if (artistType != other.artistType) return false
+        if (isPlaying != other.isPlaying) return false
+
+        return true
+    }
 }
 
 enum class ArtistType {
